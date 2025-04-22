e8da3f6b9751f7df08da7293da251f26007e6cbb
Andrea Borreca <borreca@humanbit.com>
2025-04-22 14:44:09 +0200

    Fix Metadata

--- app/Models/Metadata.php
+++ app/Models/Metadata.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Backpack\CRUD\app\Models\Traits\CrudTrait;
-use App\Services\MetadataExportService;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
@@ -13,17 +12,6 @@ class Metadata extends Model
 	use HasFactory;
 
 	protected $fillable = ["it", "en", "image_path", "code", "page_id"];
-
-	protected static function booted()
-	{
-		static::saved(function () {
-			MetadataExportService::exportMetadata();
-		});
-
-		static::deleted(function () {
-			MetadataExportService::exportMetadata();
-		});
-	}
 
 	public function page()
 	{
