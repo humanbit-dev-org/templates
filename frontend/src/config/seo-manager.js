b2ea00df2f101de1fc269a303a6453b8ecf7abcc
g-silveri <70106966+g-silveri@users.noreply.github.com>
2025-07-04 12:18:40 +0200

    refactor(seo): rename constants name
    * Update all SEO metadata fallbacks to use APP_NAME and APP_URL constant
    
    * Simplify naming convention for app name reference

--- frontend/src/config/seo-manager.js
+++ frontend/src/config/seo-manager.js
@@ -10,30 +10,30 @@ export default async function SeoManager(metadataJson, lang) {
 	return {
 		title: metadataJson.find((item) => item.code === "title")
 			? metadataJson.find((item) => item.code === "title")[lang]
-			: constants.BASE_APP_NAME,
+			: constants.APP_NAME,
 
 		description: metadataJson.find((item) => item.code === "description")
 			? metadataJson.find((item) => item.code === "description")[lang]
-			: constants.BASE_APP_NAME,
+			: constants.APP_NAME,
 
 		openGraph: {
 			url: metadataJson.find((item) => item.code === "og_url")
 				? metadataJson.find((item) => item.code === "og_url")[lang]
-				: constants.BASE_APP_URL,
+				: constants.APP_URL,
 
 			type: "website",
 
 			siteName: metadataJson.find((item) => item.code === "og_site_name")
 				? metadataJson.find((item) => item.code === "og_site_name")[lang]
-				: constants.BASE_APP_NAME,
+				: constants.APP_NAME,
 
 			title: metadataJson.find((item) => item.code === "og_title")
 				? metadataJson.find((item) => item.code === "og_title")[lang]
-				: constants.BASE_APP_NAME,
+				: constants.APP_NAME,
 
 			description: metadataJson.find((item) => item.code === "og_description")
 				? metadataJson.find((item) => item.code === "og_description")[lang]
-				: constants.BASE_APP_NAME,
+				: constants.APP_NAME,
 
 			images: metadataJson.find((item) => item.code === "og_image")
 				? metadataJson.find((item) => item.code === "og_image").image_path
