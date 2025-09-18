cce568794f66da19351b29baad1b51fc6885077b
Andrea Borreca <72610350+andreaborreca@users.noreply.github.com>
2025-09-18 20:15:15 +0200

    Add backend API token authentication
    - Update authentication flow to use API token (Bearer) instead of session cookies and CSRF tokens (without removing it).
    - Modify CORS configuration to support token-based auth.
    - Update SignIn component to store API token in a cookie, and adjusted user fetching logic to use the Authorization header.
    - Add Scramble API docs security schemes and related configuration in the backend.

--- backend/app/Providers/AppServiceProvider.php
+++ backend/app/Providers/AppServiceProvider.php
@@ -2,11 +2,20 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Dedoc\Scramble\Scramble;
+use Laravel\Sanctum\Sanctum;
 use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Sanctum\PersonalAccessToken;
+use Dedoc\Scramble\Support\Generator\OpenApi;
 use Illuminate\Auth\Notifications\ResetPassword;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Dedoc\Scramble\Support\Generator\SecurityScheme;
+use Dedoc\Scramble\Support\Generator\SecurityRequirement;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -31,6 +40,38 @@ class AppServiceProvider extends ServiceProvider
 		URL::forceRootUrl($appUrl);
 		ResetPassword::createUrlUsing(function (object $notifiable, string $token) use ($frontendUrl) {
 			return $frontendUrl . "?token=$token&email={$notifiable->getEmailForPasswordReset()}";
+		});
+
+		JsonResource::withoutWrapping();
+
+		//Scramble
+		Gate::define("viewApiDocs", function (?User $user) {
+			// consenti in questi ambienti
+			if (App::environment(["local", "development", "staging", "production"])) {
+				return true;
+			}
+
+			// consenti a utenti specifici in prod
+			return in_array(
+				optional($user)->email,
+				[
+					"solia@humanbit.com", // aggiungi altri se serve
+				],
+				true
+			);
+		});
+
+		Scramble::configure()->withDocumentTransformers(function (OpenApi $openApi) {
+			$openApi->components->securitySchemes["SwaggerBearer"] = SecurityScheme::http("bearer", "Sanctum PAT");
+			$openApi->components->securitySchemes["SwaggerBearer"]->description =
+				"USARE SOLO IN SWAGGER: incolla un Personal Access Token Sanctum (Authorization: Bearer ...)";
+			$openApi->components->securitySchemes["php_session"] = SecurityScheme::apiKey("header", "cookie");
+			$openApi->security[] = new SecurityRequirement([
+				"SwaggerBearer" => ["closed=3ganyaHkaZrey6fjgYzHJ7RImWra27Ed6os4uPuHf0ff7beb"],
+			]);
+			$openApi->security[] = new SecurityRequirement([
+				"php_session" => ["php_session=3ganyaHkaZrey6fjgYzHJ7RImWra27Ed6os4uPuHf0ff7beb"],
+			]);
 		});
 	}
 }
--- backend/config/cors.php
+++ backend/config/cors.php
@@ -14,15 +14,33 @@ return [
     |
     */
 
+	/*
+	|--------------------------------------------------------------------------
+	| Sanctum Authentication
+	|--------------------------------------------------------------------------
+	|
+	| Sanctum provide two different authentication methods:
+	| - Session Cookie
+	| - Personal Access Token
+	|
+	| The Session Cookie is the default authentication method and is used when
+	| the user is logged in.
+	|
+	| The Personal Access Token is used when the user is not logged in.
+	|
+	| - When Session Cookie is used, uncomment all paths and put supports_credentials to true
+	| - When Personal Access Token is used, comment all paths except api/* and put supports_credentials to false
+	*/
+
 	"paths" => [
 		"api/*",
-		"login",
-		"logout",
-		"register",
-		"sanctum/csrf-cookie",
-		"/email/verification-notification",
-		"forgot-password",
-		"reset-password",
+		// "login",
+		// "logout",
+		// "register",
+		// "sanctum/csrf-cookie",
+		// "/email/verification-notification",
+		// "forgot-password",
+		// "reset-password",
 	],
 
 	"allowed_methods" => ["*"],
@@ -31,11 +49,11 @@ return [
 
 	"allowed_origins_patterns" => [],
 
-	"allowed_headers" => ["*"],
+	"allowed_headers" => ["Content-Type", "Accept", "Authorization", "X-Locale"],
 
 	"exposed_headers" => [],
 
 	"max_age" => 0,
 
-	"supports_credentials" => true,
+	"supports_credentials" => false, // true for Session Cookie, false for Personal Access Token
 ];
